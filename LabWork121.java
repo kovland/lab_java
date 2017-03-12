11aefcf62fc14dc25dff436a93ceb9c71efa09c6
kovland <andriykovaliv@gmail.com>
2017-03-13 00:21:58 +0300

    Add files via upload

---
+++ LabWork121.java
@@ -0,0 +1,23 @@
+
+package lab.work.pkg1.pkg2.pkg1;
+
+
+public class LabWork121 {
+
+    public static void main(String[] args) {
+System.out.println("******       *     *           *     ");
+System.out.println("     *      * *     *         *      ");
+System.out.println("     *     *   *     *       *       ");
+System.out.println("     *    *     *     *     *        ");
+System.out.println("     *   *********     *   *         ");
+System.out.println("*    *   *       *      * *          ");
+System.out.println(" ****    *       *       *           ");
+
+
+
+
+        
+                                                    
+    }
+    
+}
