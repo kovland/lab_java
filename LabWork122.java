e8e8f4c1e2d2fbb4e87fa6211925749baed19728
kovland <andriykovaliv@gmail.com>
2017-03-13 01:50:27 +0300

    Add files via upload

---
+++ LabWork122.java
@@ -0,0 +1,27 @@
+
+package lab.work.pkg1.pkg2.pkg2;
+
+import java.util.Scanner;
+public class LabWork122 {
+
+    
+    public static void main(String[] args) {
+        
+     String s1, s2;
+     
+        
+        System.out.println("Введіть своє ім'я");
+        Scanner sc = new Scanner(System.in);
+        
+        s1 = sc.nextLine();
+        System.out.println("Введіть своє прізвище");
+        s2 = sc.nextLine();
+       
+
+        
+
+        
+        
+    }
+    
+}
